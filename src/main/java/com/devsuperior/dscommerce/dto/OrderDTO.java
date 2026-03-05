f093b57c925386f4cefe3681e9213b008f822226
Leonardo Alvarenga <leonardoalvarengacordeiro@gmail.com>
2026-03-04 22:05:04 -0300

    Saving order

--- src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.dto;
 import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 import com.devsuperior.dscommerce.entities.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -16,7 +17,11 @@ public class OrderDTO {
     private UserMinDTO client;
     private PaymentDTO payment;
 
+    @NotEmpty(message = "O pedido deve ter pelo menos um item")
     List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO() {
+    }
 
     public OrderDTO(Long id, Instant moment, OrderStatus status, UserMinDTO client, PaymentDTO payment) {
         this.id = id;
